2932dde90152e7784aa99b904e1db278c77fdfd3
Marcin Grzywaczewski <killavus@gmail.com>
2016-03-24 03:04:40 +0100

    Copied the resulting HTML from the view into a component.
    This should render the same structure as the original view. It still
    doesn't work, because of CSRF token thing - but it's a good start. The
    next steps would be to slowly transform this code to be more and more structured and not depending on in-place constants.
    
    The HTML copied needed only small modifications, namely:
    
    * Replaced all `class` attributes to `className` properties.
    * Replaced all `for` attributes to `htmlFor` properties.
    
    This is because both of them are reserved JavaScript words, thus can't
    be used as property names.

--- app/assets/javascripts/components/NewProduct.js.jsx
+++ app/assets/javascripts/components/NewProduct.js.jsx
@@ -4,7 +4,41 @@ var $ = window.$;
 
 var NewProduct = React.createClass({
   render: function () {
-    return <p>Hello, World!</p>;
+    return (<div>
+      <h1>New Product</h1>
+      <form className="new_product"
+            id="new_product"
+            action="/products"
+            acceptCharset="UTF-8"
+            method="post">
+        <input name="utf8"
+               type="hidden"
+               value="✓" />
+        <input type="hidden"
+               name="authenticity_token"
+               value="hmVLtrYUHZ4/AK2IjtxReyPdVYNwI0MoqHwkJB2dpRDLSIUFM8R2ftPjqnC5TH2lKHo5Opta7KB583hLchywJA==" />
+        <div className="field">
+          <label htmlFor="product_title">Title</label>
+          <br />
+          <input type="text"
+                 name="product[title]"
+                 id="product_title" />
+        </div>
+        <div className="field">
+          <label htmlFor="product_price">Price</label>
+          <br />
+          <input type="number"
+                 name="product[price]"
+                 id="product_price" />
+        </div>
+        <div className="actions">
+          <input type="submit"
+                 name="commit"
+                 value="Create Product" />
+        </div>
+      </form>
+      <a href="/products">Back</a>
+    </div>);
   }
 });
 
