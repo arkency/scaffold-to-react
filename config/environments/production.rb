c7c19680140157ef2ef9319d8153b61852fa99c6
Marcin Grzywaczewski <killavus@gmail.com>
2016-03-29 09:35:13 +0200

    Fixed bug: On production Rails should use production builds.
    This fixes this issue:
    https://github.com/arkency/scaffold-to-react/issues/2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,6 +77,6 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.react.variant = :development
+  config.react.variant = :production
   config.react.addons = true
 end
